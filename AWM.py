6863ff15dc83a341ed497ba7adb8cb341f3fb828
Jake Yoo <jyoo2001@gmail.com>
2023-02-06 07:49:49 -0600

    Added changes for design purposes

--- AWM.py
+++ AWM.py
@@ -4,28 +4,93 @@ app = Flask(__name__)
 from datetime import datetime;
 
 data = []
+filterBackup = []
 
-@app.route("/test/cmb-status")
+@app.route("/test/cmb/status")
 def table():
     if request.method == 'GET':
         return render_template('table.html')
 
-@app.route("/api/cmb-status", methods=['GET', 'POST'])
+@app.route("/api/cmb/status", methods=['GET', 'POST'])
 def respond():
+    global data
     if request.method =='GET':
         return jsonify(data)
     elif request.method == 'POST':
         entry = request.json
         entry['time'] = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+        if 'message' in entry:
+            step_string = """
+            {{
+                "name": "{0}",
+                "status": "{1}",
+                "time": "{2}",
+                "message": "{3}"
+            }}
+            """.format(entry['step'], entry['status'], entry['time'], entry['message'])
+        else:
+            step_string = """
+            {{
+                "name": "{0}",
+                "status": "{1}",
+                "time": "{2}"
+            }}
+            """.format(entry['step'], entry['status'], entry['time'])
+        entry_string = """
+        {{
+            "mac-address": "{0}",
+            "steps": [
+                {1}
+            ]
+        }}
+        """.format(entry['mac-address'], step_string)
         if data == []:
-            data.append(entry)
+            data.append(json.loads(entry_string))
+            return json.dumps(data)
         for i in data:
-            if i['macAddress'] == entry['macAddress']:
-                if i['status'] != entry['status']:
-                    data.remove(i) #Should remove first occurence, meaning latest status update will be retained
-                    data.append(entry)
-                    return json.dumps(data) #exit since we add an entry
-                else:
-                    return json.dumps(data) #exit since we don't want to add an entry with same mac address and status
-        data.append(entry) #if entry has different mac address, we add to table
+            if i['mac-address'] == entry['mac-address']:
+                for j in i['steps']:
+                    if j['name'] == entry['step']:
+                        i['steps'].remove(j) #Should remove first occurence, meaning latest status update will be retained
+                        i['steps'].append(json.loads(step_string))
+                        return json.dumps(data) #exit since we add an entry
+                    else: #different step, we should add entry
+                        i['steps'].append(json.loads(step_string))
+                        return json.dumps(data)
+        data.append(json.loads(entry_string)) #if entry has different step or different mac address, we add to table
         return json.dumps(data)
+
+@app.route("/api/cmb/clear", methods=['POST'])
+def clear():
+    global data
+    data = []
+    return data
+
+#FILTER WIP
+#@app.route("/api/cmb/filter")
+#def filter():
+#    input = request.args.get('input')
+#    global data
+#    global filterBackup
+#    flag = False
+#
+#    print(input)
+#
+#    if input == '':
+#        data = filterBackup
+#        return data
+#
+#    for i in filterBackup:
+#            if i['mac-address'][-4:].__contains__(input):
+#                flag = True
+#
+#    if flag == True: #If filter yields a result
+#        filterBackup = data
+#        data = []
+#        for i in filterBackup:
+#            if i['mac-address'][-4:].__contains__(input):
+#                data.append(i)
+#    else:
+#        data = []
+#    return data
+    
--- static/populate.js
+++ static/populate.js
@@ -1,41 +1,168 @@
 getData();
 async function getData() {
-    const result = await fetch("/api/cmb-status",
+    const result = await fetch("/api/cmb/status",
         {
             headers: {
                 'Accept': 'application/json'
             }
         })
-    const unfilteredData = await result.json();
-    data = unfilteredData;
+    const data = await result.json();
+    console.log(data);
     if (result.ok) {
-        /*query = document.getElementById("filter").value;
-        data = unfilteredData.filter(search, query);
-        if (!data) {
-            data = unfilteredData;
-        }
-        console.log(data);*/
+        //Clear table and remake
         $("#Boot tbody").remove();
         $("#Boot").append(`<tbody>`);
         data.forEach(i => {
-            var statusLabel;
-            if (i.status === "Started" || i.status === "Finished") {
-                statusLabel = '<i class="bi bi-check-circle-fill good-status"></i>';
-            }
-            else if (i.status === "Error") {
-                statusLabel = '<i class="bi bi-exclamation-circle-fill bad-status"></i>';
+            //Easier data access with dictionary
+            let stepDict = {
+                bootloader: undefined,
+                sdFormat: undefined,
+                sdPopulate: undefined,
+                os: undefined,
+                validate: undefined,
+                time: ""
+            };
+            let appendString = "";
+            appendString += `<tr><td class="mac-address" id="mac-address">${i["mac-address"]}</td>`;
+
+            //Initialize dictionary
+            i['steps'].forEach(j => {
+                if (j['name'] === 'bootloader') {
+                    stepDict.bootloader = j['status'];
+                    if (j['message']) {
+                        stepDict.message = j['message'];
+                    }
+                    if (stepDict.time < j['time']) {
+                        stepDict.time = j['time'];
+                    }
+                }
+                else if (j['name'] === 'sd-format') {
+                    stepDict.sdFormat = j['status'];
+                    if (j['message']) {
+                        stepDict.message = j['message'];
+                    }
+                    if (stepDict.time < j['time']) {
+                        stepDict.time = j['time'];
+                    }
+                }
+                else if (j['name'] === 'sd-populate') {
+                    stepDict.sdPopulate = j['status'];
+                    if (j['message']) {
+                        stepDict.message = j['message'];
+                    }
+                    if (stepDict.time < j['time']) {
+                        stepDict.time = j['time'];
+                    }
+                }
+                else if (j['name'] === 'os') {
+                    stepDict.os = j['status'];
+                    if (j['message']) {
+                        stepDict.message = j['message'];
+                    }
+                    if (stepDict.time < j['time']) {
+                        stepDict.time = j['time'];
+                    }
+                }
+                else if (j['name'] === 'validate') {
+                    stepDict.validate = j['status'];
+                    if (j['message']) {
+                        stepDict.message = j['message'];
+                    }
+                    if (stepDict.time < j['time']) {
+                        stepDict.time = j['time'];
+                    }
+                }
+            })
+            appendString = fillTable(stepDict, appendString);
+            if (stepDict.message) {
+                appendString += `<td>${stepDict.time}</td><td>${stepDict.message}</td></tr>`;
             }
             else {
-                statusLabel = '<i class="bi bi-exclamation-circle-fill unknown-status"></i>';
+                appendString += `<td>${stepDict.time}</td><td></td></tr>`;
             }
-            if (i.message) {
-                $("#Boot tbody").append(`<tr><td>${i.macAddress}</td><td>${i.status}&nbsp;${statusLabel}</td><td>${i.time}</td><td>${i.message}</td></tr>`);
-            }
-            else {
-                $("#Boot tbody").append(`<tr><td>${i.macAddress}</td><td>${i.status}&nbsp;${statusLabel}</td><td>${i.time}</td><td> </td></tr>`);
-            }
+            $("#Boot tbody").append(appendString);
         })
         $("#Boot").append(`</tbody>`);
+        setTimeout(getData, 1000);
     }
-    setTimeout(getData, 1000);
 }
+
+$(document).ready(function () {
+    $("#clear").click(clear)
+})
+
+function clear() {
+    $("#Boot tbody").remove();
+    const response = fetch("http://127.0.0.1:5000/api/cmb/clear", {
+        method: 'POST'
+    });
+}
+
+function fillTable(j, appendString) {
+    if (j.bootloader) {
+        statusResult = statusIcon(j.bootloader)
+        appendString += `<td>${statusResult[0]}&nbsp;${statusResult[1]}</td>`;
+    }
+    else {
+        appendString += `<td></td>`;
+    }
+    if (j.sdFormat) {
+        statusResult = statusIcon(j.sdFormat)
+        appendString += `<td>${statusResult[0]}&nbsp;${statusResult[1]}</td>`;
+    }
+    else {
+        appendString += `<td></td>`;
+    }
+    if (j.sdPopulate) {
+        statusResult = statusIcon(j.sdPopulate)
+        appendString += `<td>${statusResult[0]}&nbsp;${statusResult[1]}</td>`;
+    }
+    else {
+        appendString += `<td></td>`;
+    }
+    if (j.os) {
+        statusResult = statusIcon(j.os)
+        appendString += `<td>${statusResult[0]}&nbsp;${statusResult[1]}</td>`;
+    }
+    else {
+        appendString += `<td></td>`;
+    }
+    if (j.validate) {
+        statusResult = statusIcon(j.validate)
+        appendString += `<td>${statusResult[0]}&nbsp;${statusResult[1]}</td>`;
+    }
+    else {
+        appendString += `<td></td>`;
+    }
+    return appendString;
+}
+
+function statusIcon(inputStatus) {
+    var statusLabel;
+    var outputStatus;
+    if (inputStatus === "success") {
+        statusLabel = '<i class="bi bi-check-circle-fill good-status"></i>';
+        outputStatus = "Passed"
+    }
+    else if (inputStatus === "error") {
+        statusLabel = '<i class="bi bi-exclamation-circle-fill bad-status"></i>';
+        outputStatus = "Failed"
+    }
+    else {
+        statusLabel = '<i class="bi bi-exclamation-circle-fill unknown-status"></i>';
+        outputStatus = "Running"
+    }
+    return [statusLabel, outputStatus];
+}
+
+//FILTER WIP
+/*$(document).ready(function () {
+    $("#filter").keyup(search)
+})
+
+function search() {
+    var input = $(this).val();
+    const response = fetch("http://127.0.0.1:5000/api/cmb/filter?input=" + input, {
+        method: 'GET'
+    });
+}*/
